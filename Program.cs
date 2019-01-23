128e56744fa7ca01aa3d88580c5b5a578bdeb929
Daniel Pinheiro <nielpinheiro@gmail.com>
2019-01-23 14:09:27 -0300

    Projeto iniciado mostrando a mensagem Olá Mundo e acrescentado o readline

--- Program.cs
+++ Program.cs
@@ -6,11 +6,8 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            double[,,,] mat = new double[2, 3, 3,4];
-            Console.WriteLine(mat.Length);
-            Console.WriteLine(mat.Rank);
-            Console.WriteLine(mat.GetLength(0));
-            Console.WriteLine(mat.GetLength(1));
+            Console.WriteLine("Olá Mundo");
+            Console.WriteLine();
         }
     }
 }
