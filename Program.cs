334ae2c9c890e6bc4bd80a8ce68d9be5d51e371e
Daniel Pinheiro <nielpinheiro@gmail.com>
2019-01-23 14:08:30 -0300

    Projeto iniciado mostrando a mensagem Olá Mundo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,,,] mat = new double[2, 3, 3,4];
+            Console.WriteLine(mat.Length);
+            Console.WriteLine(mat.Rank);
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
